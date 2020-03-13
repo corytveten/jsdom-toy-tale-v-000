922ed9eb7086caa269bbf65a7b8da98855e5a86f
corytveten <corytveten@gmail.com>
2020-03-12 17:12:13 -0700

    add fetchToys function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 let addToy = false;
 
+function fetchToys(toyData) {
+  fetch('http://localhost:3000/toys', {
+      method: "GET",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        'name': toyData.name.value,
+        'image': toyData.image.value
+      })
+    })
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(json) {
+      const card = document.createElement('.card')
+      card.innerHTML = 
+    })
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyForm = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
